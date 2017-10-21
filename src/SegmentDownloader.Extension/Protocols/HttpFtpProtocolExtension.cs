814e59f34a1f5a44bf73e5bb6c1514aea0a6e2fc
golavr <golavr@gmail.com>
2017-10-21 21:47:26 +0300

    Delete redundant HttpFtpProtocolParameters and derivatives.

--- src/SegmentDownloader.Extension/Protocols/HttpFtpProtocolExtension.cs
+++ src/SegmentDownloader.Extension/Protocols/HttpFtpProtocolExtension.cs
@@ -7,8 +7,6 @@ namespace SegmentDownloader.Extension.Protocols
 {
     public class HttpFtpProtocolExtension: IExtension
     {
-        internal static IHttpFtpProtocolParameters parameters;
-
         #region IExtension Members
 
         public string Name
@@ -21,25 +19,8 @@ namespace SegmentDownloader.Extension.Protocols
             get { return new HttpFtpProtocolUIExtension(); }
         }
 
-        public HttpFtpProtocolExtension():
-            this(new HttpFtpProtocolParametersSettingsProxy())
+        public HttpFtpProtocolExtension()
         {
-        }
-
-        public HttpFtpProtocolExtension(IHttpFtpProtocolParameters parameters)
-        {
-            if (parameters == null)
-            {
-                throw new ArgumentNullException("parameters");
-            }
-
-            if (HttpFtpProtocolExtension.parameters != null)
-            {
-                throw new InvalidOperationException("The type HttpFtpProtocolExtension is already initialized.");
-            }
-
-            HttpFtpProtocolExtension.parameters = parameters;
-
             ProtocolProviderFactory.RegisterProtocolHandler("http", typeof(HttpProtocolProvider));
             ProtocolProviderFactory.RegisterProtocolHandler("https", typeof(HttpProtocolProvider));
             ProtocolProviderFactory.RegisterProtocolHandler("ftp", typeof(FtpProtocolProvider));
