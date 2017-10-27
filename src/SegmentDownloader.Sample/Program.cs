ae22009848342d512d1f261f09eda6b82d6547bd
golavr <golavr@gmail.com>
2017-10-27 10:00:22 +0300

    Update sample program.

--- src/SegmentDownloader.Sample/Program.cs
+++ src/SegmentDownloader.Sample/Program.cs
@@ -11,10 +11,10 @@ namespace SegmentDownloader.Sample
 {
     class Program
     {
-        private const string Url = "http://speedtest-ny.turnkeyinternet.net/100mb.bin";
+        private const string Url = "http://speedtest-ny.turnkeyinternet___.net/100mb.bin";
+        //private const string Url = "ftp://speedtest.tele2.net/50MB.zip";
         private const string LocalFolder = @"c:\temp";
 
-        private static Downloader _downloader;
         private static ManualResetEventSlim _manualResetEventSlim;
 
         static void Main(string[] args)
@@ -32,12 +32,12 @@ namespace SegmentDownloader.Sample
             DownloadManager.Instance.DownloadEnded += DownloadEnded;
 
             // create downloader with 8 segments
-            _downloader = DownloadManager.Instance.Add(resourceLocation, null, localFilePath, 8, false);
-            
-            // start download
-            _downloader.Start();
+            var downloader = DownloadManager.Instance.Add(resourceLocation, null, localFilePath, 8, false);
 
             _manualResetEventSlim = new ManualResetEventSlim();
+            
+            // start download
+            downloader.Start();
 
             // wait till download completed
             _manualResetEventSlim.Wait();
@@ -49,7 +49,7 @@ namespace SegmentDownloader.Sample
         private static void DownloadEnded(object sender, DownloaderEventArgs e)
         {
             Console.WriteLine(e.Downloader.State == DownloaderState.EndedWithError
-                ? "Download Ended With Error"
+                ? $"Download Ended With Error '{e.Downloader.LastError.Message}'"
                 : "Download Ended");
 
             _manualResetEventSlim.Set();
